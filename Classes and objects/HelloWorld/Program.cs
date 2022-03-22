a20e17b99a77595d874d1172d7da7b3d4e9bf7dc
Aditya Mishra <mishra.aditya50@gmail.com>
2022-03-22 19:39:25 +0100

    Classes and objects
    https://www.youtube.com/watch?v=7v1UjGqkJ1Q

---
+++ Classes and objects/HelloWorld/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Hello World, Learn tech with aditya");
+
+            Dog objDogOfJohn = new Dog();
+
+            objDogOfJohn.Name = "Bla";
+            objDogOfJohn.Breed = "Chow Chow";
+            objDogOfJohn.Age = 5;
+            objDogOfJohn.Color = "White";
+            objDogOfJohn.Owner = "John";
+
+            Dog objDogOfJony = new Dog();
+
+            objDogOfJony.Name = "GJ";
+            objDogOfJony.Breed = "German Shephard";
+            objDogOfJony.Age = 4;
+            objDogOfJony.Color = "brown";
+            objDogOfJony.Owner = "Jony";
+
+            objDogOfJohn.GetInfo();
+            objDogOfJony .GetInfo();
+
+            Console.ReadLine();
+        }
+    }
+
+   public class Dog
+    {
+        public string Name;
+        public string Breed;
+        public int Age;
+        public string Color;
+        public string Owner;
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Hello my name is: "+ Name +" , I am a "+ Breed +", my age is "+Age+", I am "+Color+" and owner is "+Owner+".");
+        }
+    }
+}
